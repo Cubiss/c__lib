4044ee075620f9b91b39a1ed700262058e8217eb
Cubiss <solin.jakub@gmail.com>
2020-05-20 07:31:25 +0200

    Added log_function to print_progress function.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if os.name == "nt":
     packages.append('pywin32')
 
 setup(name='c__lib',
-      version='0.14',
+      version='0.15',
       description='A personal collection of some useful functionalities.',
       url='https://github.com/Cubiss/c__lib',
       author='Cubiss',
