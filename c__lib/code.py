44146152688d26005196ea88dffc8d1932a1935d
Cubiss <solin.jakub@gmail.com>
2022-02-08 09:56:33 +0100

    Better code.py implementation.

--- c__lib/code.py
+++ c__lib/code.py
@@ -1,5 +1,15 @@
 def snake_case(varname: str):
+    if len(varname) < 2:
+        return varname.lower()
+
     retval = ''
+
+    while varname.startswith('_'):
+        retval += '_'
+        varname = varname[1:]
+
+    varname = varname[0].lower() + varname[1:]
+
     for c in varname:
         if c.isupper():
             retval += '_' + c.lower()
@@ -8,8 +18,28 @@ def snake_case(varname: str):
     return retval
 
 
+def pascal_case(varname: str):
+    retval = ''
+
+    while varname.startswith('_'):
+        retval += '_'
+        varname = varname[1:]
+
+    return retval + ''.join(x.capitalize() or '_' for x in snake_case(varname).split('_'))
+
+
 def camel_case(varname: str):
-    return ''.join(x.capitalize() or '_' for x in varname.split('_'))
+    if len(varname) == 1:
+        return varname.lower()
+
+    retval = ''
+
+    while varname.startswith('_'):
+        retval += '_'
+        varname = varname[1:]
+
+    pascal = pascal_case(varname)
+    return retval + pascal[0].lower() + pascal[1:]
 
 
 if __name__ == '__main__':
